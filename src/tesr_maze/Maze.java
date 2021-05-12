fc0c6ac81cfa94fb5f0eed2e62b0ef3643d7bb93
jason_yu <jason_yu@JasonYu-Win10>
2021-05-12 10:14:59 +0800

    bug fixing formatting code  function editing

---
+++ src/tesr_maze/Maze.java
@@ -0,0 +1,430 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tesr_maze;
+
+import java.io.IOException;
+import java.util.Random;
+
+/**
+ *
+ * @author jason_yu
+ */
+public class Maze {
+
+    private int x;
+    private int y;
+    private char maze[][];
+    private int cursor_x;
+    private int cursor_y;
+    private Random ran;
+    private int recur_count;
+    private int O_count;
+    private char block = 0X2588;
+    private char road = 0X25A1;
+    private char blank = 0X2591;
+    private String out="";
+
+    public Maze(int dimension) {
+        this(dimension, dimension);
+        this.x = dimension;
+        this.y = dimension;
+        ran = new Random();
+
+    }
+
+    private Maze(int dim_x, int dim_y) {
+        maze = new char[dim_x][dim_y];
+        for (int i = 0; i < dim_x; i++) {
+            for (int t = 0; t < dim_y; t++) {
+                maze[i][t] = blank;
+            }
+
+        }
+        for (int i = 0; i < dim_y; i++) {
+            maze[0][i] = block;
+            maze[dim_x - 1][i] = block;
+        }
+
+        for (int i = 0; i < dim_x; i++) {
+            maze[i][0] = block;
+            maze[i][dim_y - 1] = block;
+
+        }
+
+        maze[0][1] = road;
+        maze[1][1] = road;
+        cursor_x = 1;
+        cursor_y = 1;
+        recur_count = 0;
+
+    }
+
+    public Integer findpath() throws IOException, InterruptedException {
+      //resetRecur_count();
+ 
+        recur_count++;
+        if (recur_count >= 12000) {
+            return null;
+        }
+        if (maze[cursor_x + 1][cursor_y] != blank
+                && maze[cursor_x - 1][cursor_y] != blank
+                && maze[cursor_x][cursor_y + 1] != blank
+                && maze[cursor_x][cursor_y - 1] != blank) {
+            setpath();
+            return findpath();
+        }
+
+        int num = ran.nextInt(33339);
+        int out = num % 4;
+        if (cursor_x >= x - 1) {
+            return null;
+        }
+        if (cursor_y >= y - 1) {
+            return null;
+        }
+        if (out == 0) {
+            if (cursor_y >= y) {
+                return null;
+            } else if (cursor_x >= x) {
+                return null;
+            }
+            if (maze[cursor_x + 1][cursor_y] == block) {
+                return findpath();
+            } else if (maze[cursor_x + 1][cursor_y] == road) {
+                return findpath();
+            } else if (maze[cursor_x + 1][cursor_y] == blank) {
+                num = ran.nextInt(33339);
+                out = num % 2;
+
+                if (cursor_x >= 1
+                        && cursor_y >= 1
+                        && cursor_x <= x - 1
+                        && cursor_y <= y - 1) {
+                    if (this.check_O(cursor_x, cursor_y) >= 2) {
+                        maze[cursor_x + 1][cursor_y] = block;
+                        return findpath();
+
+                    }
+
+                }
+
+                if (out == 0) {
+                    maze[cursor_x + 1][cursor_y] = road;
+                    cursor_x++;
+
+                    return findpath();
+                } else {
+                    maze[cursor_x + 1][cursor_y] = block;
+                    return findpath();
+                }
+            }
+
+        } else if (out == 1) {
+            if (cursor_y >= y) {
+                return null;
+            } else if (cursor_x >= x) {
+                return null;
+            } else if (cursor_x <= 0) {
+                return findpath();
+            }
+            if (maze[cursor_x - 1][cursor_y] == block) {
+                return findpath();
+            } else if (maze[cursor_x - 1][cursor_y] == road) {
+                return findpath();
+            } else if (maze[cursor_x - 1][cursor_y] == blank) {
+
+                num = ran.nextInt(33339);
+                out = num % 2;
+                if (cursor_x >= 1
+                        && cursor_y >= 1
+                        && cursor_x <= x - 1
+                        && cursor_y <= y - 1) {
+                    if (this.check_O(cursor_x, cursor_y) >= 2) {
+                        maze[cursor_x - 1][cursor_y] = block;
+                        return findpath();
+
+                    }
+
+                }
+
+                if (out == 0 || this.O_count < 7) {
+                    maze[cursor_x - 1][cursor_y] = road;
+                    cursor_x--;
+
+                    return findpath();
+                } else {
+                    maze[cursor_x - 1][cursor_y] = block;
+                    return findpath();
+                }
+
+            }
+
+        } else if (out == 2) {
+            if (cursor_y >= y) {
+                return null;
+            } else if (cursor_x >= x) {
+                return null;
+            } else if (cursor_y <= 0) {
+                return findpath();
+            }
+            if (maze[cursor_x][cursor_y - 1] == block) {
+                return findpath();
+            } else if (maze[cursor_x][cursor_y - 1] == road) {
+                return findpath();
+            } else if (maze[cursor_x][cursor_y - 1] == blank) {
+
+                num = ran.nextInt(33339);
+                out = num % 2;
+                if (cursor_x >= 1
+                        && cursor_y >= 1
+                        && cursor_x <= x - 1
+                        && cursor_y <= y - 1) {
+                    if (this.check_O(cursor_x, cursor_y) >= 2) {
+                        //System.out.println(this.check_O(cursor_x, cursor_y));
+                        maze[cursor_x][cursor_y - 1] = block;
+                        return findpath();
+
+                    }
+
+                }
+
+                if (out == 0 || this.O_count < 7) {
+                    maze[cursor_x][cursor_y - 1] = road;
+                    cursor_y--;
+
+                    return findpath();
+                } else {
+                    maze[cursor_x][cursor_y - 1] = block;
+                    return findpath();
+                }
+
+            }
+
+        } else if (out == 3) {
+            if (cursor_y >= y) {
+                return null;
+            } else if (cursor_x >= x) {
+                return null;
+            }
+            if (maze[cursor_x][cursor_y + 1] == block) {
+                return findpath();
+            } else if (maze[cursor_x][cursor_y + 1] == road) {
+                return findpath();
+            } else if (maze[cursor_x][cursor_y + 1] == blank) {
+
+                num = ran.nextInt(33339);
+                out = num % 2;
+
+                if (cursor_x >= 1
+                        && cursor_y >= 1
+                        && cursor_x <= x - 1
+                        && cursor_y <= y - 1) {
+                    if (this.check_O(cursor_x, cursor_y) >= 2) {
+                        maze[cursor_x][cursor_y + 1] = block;
+                        return findpath();
+
+                    }
+
+                }
+
+                if (out == 0 || this.O_count < 7) {
+                    maze[cursor_x][cursor_y + 1] = road;
+                    cursor_y++;
+
+                    return findpath();
+                } else {
+                    maze[cursor_x][cursor_y + 1] = block;
+                    return findpath();
+                }
+
+            }
+
+        }
+
+        return null;
+
+    }
+
+    public void print_maze() {
+        System.out.println(cursor_x + " " + cursor_y);
+        for (int i = 0; i < x; i++) {
+            for (int t = 0; t < y; t++) {
+                if (t != y - 1) {
+                    out+=maze[t][i];
+                    //System.out.print(maze[t][i] + "");
+                } else if (t == y - 1) {
+                    out+=maze[t][i]+"\n";
+                   // System.out.println(maze[t][i]);
+                }
+            }
+
+        }
+    }
+    
+    public String out(){
+        return this.out;
+    }
+
+    public Integer setpath() {
+
+        for (int i = 0; i < x; i++) {
+            for (int t = 0; t < y; t++) {
+                if ((i >= 1 && t >= 1 && i < x - 1 && t < y - 1) && maze[i][t] == road) {
+                    if (maze[i + 1][t] == blank
+                            && maze[i - 1][t] != blank
+                            && maze[i][t + 1] != blank
+                            && maze[i][t - 1] != blank) {
+                        maze[i + 1][t] = road;
+                        cursor_x = i + 1;
+                        cursor_y = t;
+                        return null;
+                    } else if (maze[i + 1][t] != blank
+                            && maze[i - 1][t] == blank
+                            && maze[i][t + 1] != blank
+                            && maze[i][t - 1] != blank) {
+                        maze[i - 1][t] = road;
+                        cursor_x = i - 1;
+                        cursor_y = t;
+                        return null;
+                    } else if (maze[i + 1][t] != blank
+                            && maze[i - 1][t] != blank
+                            && maze[i][t + 1] == blank
+                            && maze[i][t - 1] != blank) {
+                        maze[i][t + 1] = road;
+                        cursor_x = i;
+                        cursor_y = t + 1;
+                        return null;
+                    } else if (maze[i + 1][t] != blank
+                            && maze[i - 1][t] != blank
+                            && maze[i][t + 1] != blank
+                            && maze[i][t - 1] == blank) {
+                        maze[i][t - 1] = road;
+                        cursor_x = i;
+                        cursor_y = t - 1;
+                        return null;
+                    }
+                } else if (i == x - 1 && t == y - 1 ) {
+                    for (int a = 1; a < x; a++) {
+                        for (int b = 1; b < y; b++) {
+                            if (maze[a][b] == block && check_O(a, b) >= 1 && check_X(a, b) > 2) {
+                                //System.out.println("1");
+                                maze[a][b] = road;
+                                cursor_x = a;
+                                cursor_y = b;
+                                return null;
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+
+        return null;
+
+    }
+
+    public int check_X(int in_x, int in_y) {
+        int result = 0;
+
+        if (in_x > 0 && in_y > 0 && in_x < x - 1 && in_y < y - 1) {
+            if (maze[in_x + 1][in_y] == block) {
+                result++;
+            }
+            if (maze[in_x][in_y + 1] == block) {
+                result++;
+            }
+            if (maze[in_x - 1][in_y] == block) {
+                result++;
+            }
+            if (maze[in_x][in_y - 1] == block) {
+                result++;
+            }
+
+        }
+        // System.out.println(result);
+        return result;
+    }
+
+    public int check_O(int in_x, int in_y) {
+        int result = 0;
+
+        if (in_x > 0 && in_y > 0 && in_x < x - 1 && in_y < y - 1) {
+            if (maze[in_x + 1][in_y] == road) {
+                result++;
+            }
+            if (maze[in_x][in_y + 1] == road) {
+                result++;
+            }
+            if (maze[in_x - 1][in_y] == road) {
+                result++;
+            }
+            if (maze[in_x][in_y - 1] == road) {
+                result++;
+            }
+
+        }
+        // System.out.println(result);
+        return result;
+    }
+
+    public int count_O() {
+        int result = 0;
+        for (int i = 0; i < x; i++) {
+            for (int t = 0; t < y; t++) {
+                if (maze[t][i] == road) {
+                    result++;
+                }
+            }
+
+        }
+
+        return result;
+    }
+    
+    public void reset_maze(){
+        for (int i = 0; i < x; i++) {
+            for (int t = 0; t < y; t++) {
+                maze[i][t] = blank;
+            }
+
+        }
+        for (int i = 0; i < y; i++) {
+            maze[0][i] = block;
+            maze[x - 1][i] = block;
+        }
+
+        for (int i = 0; i < x; i++) {
+            maze[i][0] = block;
+            maze[i][y - 1] = block;
+
+        }
+
+        maze[0][1] = road;
+        maze[1][1] = road;
+        cursor_x = 1;
+        cursor_y = 1;
+        recur_count = 0;
+    }
+    
+    public void maze_eraseblank(){
+        for(int i=0;i<x;i++){
+            for(int t=0;t<y;t++){
+                if(maze[i][t]==blank){
+                  //  System.out.println("1");
+                    maze[i][t]=block;
+                }
+                
+            }
+            
+        }
+    }
+    
+    public void resetRecur_count(){
+        this.recur_count=0;
+    }
+
+}
---
+++ src/tesr_maze/Tesr_maze.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tesr_maze;
+
+import java.io.IOException;
+import java.lang.System.Logger;
+import java.lang.System.Logger.Level;
+
+/**
+ *
+ * @author jason_yu
+ */
+public class Tesr_maze {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Maze maze = new Maze(200);
+
+        maze.findpath();
+        while (maze.count_O() < 26700) {
+            System.out.println("retry");
+            maze.resetRecur_count();
+            maze.reset_maze();
+            
+            for(int i=0;i<17;i++){
+                maze.resetRecur_count();
+                maze.findpath();
+            }
+            
+        }
+        maze.maze_eraseblank();
+
+        System.out.println(maze.count_O());
+       maze.print_maze();
+        System.out.println(maze.out());
+        
+
+        //char c = 0x2588;//aka 8706 in decimal. 
+        //  System.out.println(c);
+    }
+
+    public static int returnZero(int anyNumber) {
+        if (anyNumber == 0) {
+            return 0;
+        } else {
+            anyNumber--;
+            return returnZero(anyNumber);
+        }
+    }
+
+}
